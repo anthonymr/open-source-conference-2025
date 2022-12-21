b9de3478aac6849edf4d9c4b7b336eb1bcc6417c
Anthony Martin <anthonymartin20@gmail.com>
2022-12-21 15:46:04 +0100

    Fix linter error

--- js/form_validation.js
+++ js/form_validation.js
@@ -2,13 +2,13 @@ const contactForm = document.querySelector('#form');
 const errorMessage = document.querySelector('#error-message');
 
 contactForm.addEventListener('submit', (event) => {
-    const email = contactForm.elements['email'].value;
-    
-    if(email !== email.toLowerCase()){
-        event.preventDefault();
-        errorMessage.innerHTML = 'Email field only can include lowercase characters.';
-        errorMessage.classList.remove('hidden');
-    } else {
-        errorMessage.classList.add('hidden');
-    }
+  const email = contactForm.elements.email.value;
+
+  if (email !== email.toLowerCase()) {
+    event.preventDefault();
+    errorMessage.innerHTML = 'Email field only can include lowercase characters.';
+    errorMessage.classList.remove('hidden');
+  } else {
+    errorMessage.classList.add('hidden');
+  }
 });
