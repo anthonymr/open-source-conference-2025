470471db05863d57ec7e1ce9f267a88c21ea6945
Anthony Martin <anthonymartin20@gmail.com>
2022-12-22 14:07:35 +0100

    Fix linter problems

--- js/local_storage.js
+++ js/local_storage.js
@@ -1,24 +1,23 @@
 const fields = document.querySelectorAll('.field-data');
 
 let formData = {
-    name: '',
-    email: '',
-    message: '',
-}
+  name: '',
+  email: '',
+  message: '',
+};
 
 fields.forEach((field) => {
-    field.addEventListener('input', () => {
-        formData[field.name] = field.value;
-        localStorage.setItem('portfolio-form-data', JSON.stringify(formData));
-    });
+  field.addEventListener('input', () => {
+    formData[field.name] = field.value;
+    localStorage.setItem('portfolio-form-data', JSON.stringify(formData));
+  });
 });
 
-window.addEventListener("load", () => {
-    let storedData = localStorage.getItem('portfolio-form-data');
-    formData = JSON.parse(storedData);
+window.addEventListener('load', () => {
+  const storedData = localStorage.getItem('portfolio-form-data');
+  formData = JSON.parse(storedData);
 
-    fields.forEach((field) => {
-        field.value = formData[field.name];
-    });
+  fields.forEach((field) => {
+    field.value = formData[field.name];
+  });
 });
-
